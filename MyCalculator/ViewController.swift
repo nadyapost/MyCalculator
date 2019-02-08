d1486c2854ab22918258f2edc36bb69cd715146a
nadyapost <nadyapostr@gmail.com>
2019-02-08 12:20:30 +1100

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Nadya Postriganova on 8/2/19.
+//  Copyright © 2019 Nadya Postriganova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
